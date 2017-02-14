59e8d2131e59e0a03f022fe726ca59b4f4bd19ea
Zongmin Lei <leizongmin@gmail.com>
2017-02-14 13:19:38 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,114 @@
 # lei-async
-基于 async function 的异步库
+基于 async function 的数组相关操作异步库
+
+## 安装
+
+```bash
+$ npm install lei-async --save
+```
+
+## 使用方法
+
+```javascript
+(async function () {
+
+  // forEach
+  {
+    await async.forEach([ 1, 3, 5, 7, 9 ], async function (item, index) {
+      console.log(item, index);
+    });
+  }
+
+  // map
+  {
+    const list = await async.map([ 2, 4, 6, 8, 10 ], async function (item, index) {
+      return item + index;
+    });
+    console.log(list);
+  }
+
+  // reduce
+  {
+    const ret = await async.reduce([ 1, 3, 5, 7, 9 ], async function (a, b) {
+      return a + b;
+    });
+    console.log(ret);
+  }
+
+  // reduceRight
+  {
+    const ret = await async.reduceRight([ 1, 3, 5, 7, 9 ], async function (a, b) {
+      return a - b;
+    });
+    console.log(ret);
+  }
+
+  // filter
+  {
+    const list = await async.filter([ 1, 3, 5, 7, 9 ], async function (item) {
+      return item > 5;
+    });
+    console.log(list);
+  }
+
+  // every
+  {
+    const ok = await async.every([ 2, 4, 6, 8, 10 ], async function (item) {
+      return item % 2 === 0;
+    });
+    console.log(ok);
+  }
+
+  // some
+  {
+    const ok = await async.some([ 2, 4, 6, 8, 10 ], async function (item) {
+      return item % 2 === 0;
+    });
+    console.log(ok);
+  }
+
+  // find
+  {
+    const ret = await async.find([ 1, 3, 5, 7, 9 ], async function (item) {
+      return item > 5;
+    });
+    console.log(ret);
+  }
+
+  // findIndex
+  {
+    const ret = await async.findIndex([ 1, 3, 5, 7, 9 ], async function (item) {
+      return item > 5;
+    });
+    console.log(ret);
+  }
+
+})();
+```
+
+
+## License
+
+```
+MIT License
+
+Copyright (c) 2017 Zongmin Lei <leizongmin@gmail.om>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
---
+++ test/test.js
@@ -0,0 +1,119 @@
+/**
+ * lei-async tests
+ *
+ * @author Zongmin Lei <leizongmin@gmail.com>
+ */
+
+const expect = require('chai').expect;
+const async = require('../');
+
+function sleep(num) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, num);
+  });
+}
+
+describe('test lei-async', function () {
+
+  it('forEach', async function () {
+    const items = [];
+    const indexes = [];
+    await async.forEach([ 1, 3, 5, 7, 9 ], async function (item, index) {
+      items.push(item);
+      indexes.push(index);
+      await sleep(10);
+    });
+    expect(items).to.eql([ 1, 3, 5, 7, 9 ]);
+    expect(indexes).to.eql([ 0, 1, 2, 3, 4 ]);
+  });
+
+  it('map', async function () {
+    const list = await async.map([ 2, 4, 6, 8, 10 ], async function (item, index) {
+      await sleep(10);
+      return item + index;
+    });
+    expect(list).to.eql([ 2, 4 + 1, 6 + 2, 8 + 3, 10 + 4 ]);
+  });
+
+  it('reduce', async function () {
+    const ret = await async.reduce([ 1, 3, 5, 7, 9 ], async function (a, b) {
+      await sleep(10);
+      return a + b;
+    });
+    expect(ret).to.equal(1 + 3 + 5 + 7 + 9);
+    expect(ret).to.equal([ 1, 3, 5, 7, 9 ].reduce((a, b) => a + b));
+  });
+
+  it('reduceRight', async function () {
+    const ret = await async.reduceRight([ 1, 3, 5, 7, 9 ], async function (a, b) {
+      await sleep(10);
+      return a - b;
+    });
+    expect(ret).to.equal(9 - 7 - 5 - 3 - 1);
+    expect(ret).to.equal([ 1, 3, 5, 7, 9 ].reduceRight((a, b) => a - b));
+  });
+
+  it('filter', async function () {
+    const list = await async.filter([ 1, 3, 5, 7, 9 ], async function (item) {
+      await sleep(10);
+      return item > 5;
+    });
+    expect(list).to.eql([ 7, 9 ]);
+  });
+
+  it('every', async function () {
+    {
+      const ok = await async.every([ 2, 4, 6, 8, 10 ], async function (item) {
+        await sleep(10);
+        return item % 2 === 0;
+      });
+      expect(ok).to.be.true;
+    }
+    {
+      const ok = await async.every([ 2, 4, 6, 7, 8 ], async function (item) {
+        await sleep(10);
+        return item % 2 === 0;
+      });
+      expect(ok).to.be.false;
+    }
+  });
+
+  it('some', async function () {
+    {
+      const ok = await async.some([ 2, 4, 6, 8, 10 ], async function (item) {
+        await sleep(10);
+        return item % 2 === 0;
+      });
+      expect(ok).to.be.true;
+    }
+    {
+      const ok = await async.some([ 2, 4, 6, 7, 8 ], async function (item) {
+        await sleep(10);
+        return item % 2 === 0;
+      });
+      expect(ok).to.be.true;
+    }
+    {
+      const ok = await async.some([ 1, 3, 5, 7, 9 ], async function (item) {
+        await sleep(10);
+        return item % 2 === 0;
+      });
+      expect(ok).to.be.false;
+    }
+  });
+
+  it('find', async function () {
+    const ret = await async.find([ 1, 3, 5, 7, 9 ], async function (item) {
+      return item > 5;
+    });
+    expect(ret).to.equal(7);
+  });
+
+  it('findIndex', async function () {
+    const ret = await async.findIndex([ 1, 3, 5, 7, 9 ], async function (item) {
+      return item > 5;
+    });
+    expect(ret).to.equal(3);
+  });
+
+});
